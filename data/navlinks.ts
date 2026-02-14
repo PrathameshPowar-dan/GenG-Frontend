85febc629e4c7e454005c0f4b3abeb6eec06203f
PrathameshPowar-dan <powarprathamesh579@gmail.com>
2026-02-15 02:01:44 +0530

    New

---
+++ data/navlinks.ts
@@ -0,0 +1,8 @@
+import { INavLink } from "@/types";
+
+export const navlinks: INavLink[] = [
+    { name: "Home", href: "/" },
+    { name: "Create", href: "/create" },
+    { name: "My Clothes", href: "/my-clothes" },
+    { name: "FAQ", href: "/FAQ" },
+];
